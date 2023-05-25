97e67c6e05261ae53ed46e2e8647e2e8a03a4032
Skarcher29 <samanthakarcher@gmail.com>
2023-05-25 15:07:37 -0500

    finished exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            var div = a/b;
+            var mod = a%b;
+
+            if (a==17 && b==4)
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}"); 
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with a radius of {r} is {areaOfCircle}");
         }
     }
 }
